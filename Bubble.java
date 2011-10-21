b0a195dc17eead0bc23bc300005b2f2ea18e582d
Do Kook Choe <cdg720@gmail.com>
2011-10-20 21:28:14 -0400

    add features to Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Bubble{
     String name;
     String[] methods;
     String[] dataFields;
     Bubble parent;
     String[] children;
+    ArrayList<String> tempChildren;
     public Bubble(String name, String[] methods,
             String[] dataFields, Bubble parent, String[] children){
 
@@ -12,5 +15,34 @@ public class Bubble{
         this.dataFields = dataFields;
         this.parent = parent;
         this.children = children;
+    }
+
+    public Bubble(String name, String child) {
+	this.name = name;
+	tempChildren = new ArrayList<String>();
+	tempChildren.add(child);
+    }
+
+    public void setMethods(String[] methods) {
+	this.methods = methods;
+    }
+
+    public void setDataFields(String[] dataFields) {
+	this.dataFields = dataFields;
+    }
+
+    public void setParent(Bubble parent) {
+	this.parent = parent;
+    }
+    
+    public void setChildren(String[] children) {
+	this.children = children;
+    }		       
+
+    public void addChild(String child) {
+	if (tempChildren == null) {
+	    tempChildren = new ArrayList<String>();
+	}
+	tempChildren.add(child);
     }
 }
