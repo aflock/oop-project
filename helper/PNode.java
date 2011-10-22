a888bf9714edc56b25f58df30d6f0b5584c9a233
aflock <andrewbflockhart@gmail.com>
2011-10-22 17:56:07 -0400

    Add PNode to helpers

---
+++ helper/PNode.java
@@ -0,0 +1,12 @@
+package xtc.oop.helper;
+
+public class PNode{
+    String name;
+    PNode[] packageChildren;
+    String[] structChildren;
+
+    public PNode(String name){
+        this.name = name;
+    }
+
+}
