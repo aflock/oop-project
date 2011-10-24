aeb365b22f14b1e61d39bd237162ddbcb5ca98d5
Calvin Hawkes <calvinhawkes@gmail.com>
2011-10-23 22:33:13 -0400

    changed Decl

--- helper/Mubble.java
+++ helper/Mubble.java
@@ -6,17 +6,20 @@ public class Mubble{
      String methName; //name of the method
      String name; //class method is in
      String code; //actual code of class, in Block() type node of AST
+     String packageName;
      
      public Mubble(String iName, String iHeader)
      {
         this.name = iName;
         this.header = formatMethodHeader(iHeader);
-        System.out.println("========HEADER: " + iHeader);
-        System.out.println("========HEADER: " + header);
         this.methName = extractMethodName(iHeader);
         this.code = "";
      }
      
+     public void setPackageName(String pack)
+     {
+        this.packageName = pack;
+     }
      
      public String extractMethodName(String in)
      {
