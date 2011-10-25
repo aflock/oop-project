299cece4616afb2d8a91c65bb141b06ff93e84f3
aflock <andrewbflockhart@gmail.com>
2011-10-25 02:01:14 -0400

    fix tabs HOPEFULLY

--- Decl.java
+++ Decl.java
@@ -732,10 +732,10 @@ public class Decl extends xtc.util.Tool
         Bubble object = new Bubble("Object", null);
         //Creating Object's Vtable
         object.add2Vtable("Class __isa;");
-        object.add2Vtable("int32_t (*hashCode)(Object);\t");
-        object.add2Vtable("bool (*equals)(Object, Object);\t");
-        object.add2Vtable("Class (*getClass)(Object);\t");
-        object.add2Vtable("String (*toString)(Object);\t");
+        object.add2Vtable("int32_t (*hashCode)(Object);");
+        object.add2Vtable("bool (*equals)(Object, Object);");
+        object.add2Vtable("Class (*getClass)(Object);");
+        object.add2Vtable("String (*toString)(Object);");
         bubbleList.add(object);
 
 
@@ -743,10 +743,10 @@ public class Decl extends xtc.util.Tool
         Bubble string = new Bubble("String", null);
         //Creating Object's Vtable
         string.add2Vtable("Class __isa;");
-        string.add2Vtable("int32_t (*hashCode)(String);\t");
-        string.add2Vtable("bool (*equals)(String, Object);\t");
+        string.add2Vtable("int32_t (*hashCode)(String);");
+        string.add2Vtable("bool (*equals)(String, Object);");
         string.add2Vtable("Class (*getClass)(String);");
-        string.add2Vtable("String (*toString)(String);\t");
+        string.add2Vtable("String (*toString)(String);");
         string.add2Vtable("int32_t (*length)(String);");
         string.add2Vtable("char (*charAt)(String, int_32_t);");
         bubbleList.add(string);
@@ -1221,6 +1221,9 @@ public class Decl extends xtc.util.Tool
         //Object m : b.getVtable().toArray()
         ArrayList<String> vtable = parent.getVtable();
         String entry = vtable.get(index);
+        if(parent.getParent() == null){
+            return parent.getName();
+        }
         if(entry.charAt(entry.length()-1)=='\t'){
             return parent.getName();
         }
