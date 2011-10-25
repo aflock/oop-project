8d413a48b9a00642f720ddb9016ba44db1f7c30a
Calvin Hawkes <calvinhawkes@gmail.com>
2011-10-25 08:35:00 -0400

    omfg it compiles

---
+++ Test.java
@@ -0,0 +1,24 @@
+public class Test
+{
+
+    public Test()
+    {
+    }
+
+    public static void main(String[] args)
+    {
+        int a = 3;
+        int b = 5;
+        int[] test = new int[5];
+        test[0] = 5;
+        String[] test2 = new String[5];
+        Test noob = new Test();
+        noob.rage(a,b);
+    }
+    
+    public int rage(int a, int b)
+    {
+        return 69;
+    }
+}
+
