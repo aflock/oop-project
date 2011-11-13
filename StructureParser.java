8f60d9f7aaa67147bc8d310c157aea4ee4ea27d6
aflock <andrewbflockhart@gmail.com>
2011-11-13 18:28:09 -0500

    adding skeleton methods to parsers

--- StructureParser.java
+++ StructureParser.java
@@ -33,7 +33,7 @@ public class StructureParser extends xtc.tree.Visitor //aka Decl
     public static ArrayList<PNode> packageTree;
     public static ArrayList<Mubble> mubbleList;
     public static ArrayList<String> parsed; //keeps track of what ASTs have been parsed
-    
+
     public StructureParser(ArrayList<Pubble> packageTree, ArrayList<Mubble> mubbleList, ArrayList<Bubble> bubbleList, ArrayList<String> parsed)
     {
         this.packageTree = packageTree;
@@ -42,13 +42,116 @@ public class StructureParser extends xtc.tree.Visitor //aka Decl
         this.parsed = parsed;
     }
 
-        
+
     public void visitClassDeclaration(GNode n){
         //n.getString(0) is the Modifiers node
         //n.getString(1) is the name of the class
         bubbleList.add(new Bubble()); //Create New Bubble which whatever constructor we decide
-        
+
     }
 
+
+
+    public void visitFieldDeclaration(GNode n){
+        visit(n);
+    }
+
+    public void visitDimensions(GNode n) {
+        visit(n);
+    }
+
+    public void visitModifiers(GNode n){
+        visit(n);
+
+    }
+
+    public void visitMethodDeclaration(GNode n){
+        visit(n);
+
+        /*
+         *
+         */
+
+        /*
+        methods.add("");
+        visit(n);
+        String name = n.getString(3);
+        if (name == "static")
+            name = name + " " + n.getString(4);
+        methods.set(methods.size()-1,methods.get(methods.size()-1)+" "+name);
+        */
+
+    }
+
+    public void visitModifier(GNode n){
+        visit(n);
+
+    }
+
+    public void visitDeclarators(GNode n) {
+        visit(n);
+    }
+
+    public void visitDeclarator(GNode n) {
+        visit(n);
+    }
+
+    public void visitIntegerLiteral(GNode n) {
+        visit(n);
+    }
+
+    public void visitClassBody(GNode n){
+        visit(n);
+    }
+
+    public void visitClassDeclaration(GNode n){
+        visit(n);
+    }
+
+    public void visitFormalParameters(GNode n){
+        visit(n);
+    }
+
+    public void visitFormalParameter(GNode n) {
+
+        visit(n);
+    }
+
+    public void visitQualifiedIdentifier(GNode n){
+        visit(n);
+    }
+
+    public void visitImportDeclaration(GNode n){
+        visit(n);
+    }
+
+    public void visitForStatement(GNode n)
+    {
+        visit(n);
+    }
+
+    public void visitBasicForControl(GNode n)
+    {
+        visit(n);
+    }
+
+    public void visitPrimitiveType(GNode n) {
+        visit(n);
+    }
+
+    public void visitType(GNode n)
+    {
+        visit(n);
+    }
+
+    public void visitExpressionList(GNode n)
+    {
+        visit(n);
+    }
+
+    public void visitRelationalExpression(GNode n)
+    {
+        visit(n);
+    }
 
 }
