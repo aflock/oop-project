d3d63e0b77e5b65ec933b59b43a811ba315b6928
Do Kook Choe <cdg720@gmail.com>
2011-11-29 00:31:47 -0500

    Shit.java

---
+++ tests/Shit.java
@@ -0,0 +1,21 @@
+public class Shit {
+    public Shit() {
+
+    }
+    
+    public static void main(String[] args) {
+	Shit c = new Shit();
+	
+	int a, b;
+	System.out.println(c.n().m(a=b=1));
+	
+    }
+
+    Shit n() {
+	return this;
+    }
+
+    int m(int a) {
+	return a;
+    }
+}
