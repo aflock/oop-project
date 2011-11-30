38ea50b28d43b7f11d0c27abfe1a437bcc7a438f
Do Kook Choe <cdg720@gmail.com>
2011-11-29 22:43:10 -0500

    working testFor.h & .cc

--- tests/translated/testFor.cc
+++ tests/translated/testFor.cc
@@ -1,24 +1,28 @@
 #include "testFor.h"
+
 #include <iostream>
 
 using namespace std;
 
-_testFor::_testFor(): __vptr(&__vtable){} 
+_testFor::_testFor() : __vptr(&__vtable){
+}
 
-
+// compile: g++ testFor.cc java_lang.cc
 
 int main(void) {
-cout << "Running for loop" << endl;
-for(int i = 0; i < 5; i++) {
-cout << "for loop i: " << i << endl;
+  cout << "Running for loop" << endl;
+  for(int i = 0; i < 5; i++) {
+    cout << "for loop i: " << i << endl;
+  }
+  return 0;
 }
-return 0;
+
+Class _testFor::__class() {
+  static Class k = 
+    new java::lang::__Class(__rt::literal("testFor"), java::lang::__Object::__class());
+  return k;
 }
 
 _testFor_VT _testFor::__vtable;
 
-Class _testFor::__class() { 
- static Class k = new java::lang::__Class(__rt::literal("testFor"), java::lang::__Object::__class());
-return k;
-}
 
