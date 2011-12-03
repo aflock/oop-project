6b5584a0b027d7de875c08f97947aa3e8d7b70e7
aflock <andrewbflockhart@gmail.com>
2011-12-03 14:11:29 -0500

    short plus short test

---
+++ tests/testShortplusShort.java
@@ -0,0 +1,23 @@
+public class testShortplusShort{
+    public static void main (String [] args)
+    {
+        int a = 2;
+        int b = 2;
+
+        short c = 4;
+        short d = 4;
+
+        System.out.println(checker(a + b));
+        System.out.println(checker(c + d));
+        System.out.println(checker(a));
+        System.out.println(checker(d));
+
+    }
+    public static String checker(short in){
+        return "short param";
+    }
+    public static String checker(int in){
+        return "int param";
+    }
+
+}
