39a4f72538faae5a2b4bdfca936abd03f8122fb1
aflock <andrewbflockhart@gmail.com>
2011-12-06 18:47:46 -0500

    add test case

---
+++ tests/testPrintingWays.java
@@ -0,0 +1,28 @@
+public class testPrintingWays{
+    public static void main (String [] args)
+    {
+        System.out.println("String one plus" + " String 2");
+        System.out.println("String plus integer " + 4);
+        System.out.println(new Object() + " and a string and int" + 4);
+        System.out.println(5+5);
+        System.out.println(5.0 + 5.5 + "55.5");
+        System.out.println("55" + 6 + 6);
+        System.out.println('a');
+        System.out.print("print without a new line");
+        System.out.print("print without a new line");
+        System.out.print("print without a new line");
+        System.out.println(new String("abcd").charAt(2) + "asdf" + 5);
+        //System.out.println(new grimmsclass().printwithincreasedcount());
+        //System.out.println(grimsclass.getCount());
+    }
+}
+/*
+String one plus String 2
+String plus integer 4
+java.lang.Object@635b9e68 and a string and int4
+10
+10.555.5
+5566
+a
+print without a new lineprint without a new lineprint without a new linec
+*/
