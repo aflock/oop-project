b86c67af9f5724e14e99c14022d21b581a3292d4
aflock <andrewbflockhart@gmail.com>
2011-12-15 21:00:23 -0500

    test is more gnarly

--- tests/testDynamicStaticTypeDifferent.java
+++ tests/testDynamicStaticTypeDifferent.java
@@ -5,18 +5,41 @@ public class testDynamicStaticTypeDifferent{
     public static void m(Object c){
         System.out.println("Obj");
     }
+    public static void m(B b){
+         System.out.println("BBB");
+    }
+    public static void m(A b){
+         System.out.println("AAB");
+    }
     public static void main (String [] args)
     {
         Object a = "Ahhhh";
         m(a);
-        A example = new B();
+        A example  = new B();
+        B example2 = new B();
+        A example3 = example2;
         example.method();
+        example2.method();
+        example3.method();
         example.dynMeth();
-
+        example2.dynMeth();
+        example3.dynMeth();
+        m(example);
+        m(example2);
+        m(example3);
+        example.dynMeth(example);
+        example.dynMeth(example2);
+        example.dynMeth(example3);
     }
 }
 
 class A{
+    public void dynMeth(B a){
+        System.out.println(" argument bb");
+    }
+    public void dynMeth(A a){
+        System.out.println(" argument aa");
+    }
     public void dynMeth(){
         System.out.println("Dynamix a");
     }
@@ -32,3 +55,6 @@ class B extends A{
         System.out.println("B method");
     }
 }
+/* How do we solve this problem?
+ * Any time a new object is created with NewClassExpression, wanf
+ */
